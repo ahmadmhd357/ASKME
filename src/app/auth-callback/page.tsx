4e8020ef1b3ffe2c71141c1a6cc45d922b9027da
Ahmad Alhariri <ahmadmhd357@gmail.com>
2024-12-02 17:06:58 +0300

    trpc integration

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,9 +1,16 @@
 import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
 
 const Page = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get('origin');
+  
+  const {data, isLoading} = trpc.authCallback.useQuery(undefined)
+  
+  if(data?.success){
+    router.push(origin? `/${origin}` :'/dashboard')
+  }
   
 }
 
